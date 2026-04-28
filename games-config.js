f4e2a0ba83d0ced6b2cf6901db868fd02d38f161
devAdaid <ahyeong2533@gmail.com>
2026-04-28 20:14:34 +0900

    초기 커밋

---
+++ games-config.js
@@ -0,0 +1,9 @@
+// 게임을 추가하려면 이 배열에 항목을 추가하세요.
+window.GAMES = [
+  {
+    "id": "sample-game",
+    "title": "샘플 게임",
+    "thumbnail": "thumbnails/sample-game.png",
+    "description": "게임 한 줄 소개를 여기에 씁니다."
+  }
+];
